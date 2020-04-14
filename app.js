a644810d4d80f43f310329c9010d82679dcb3a31
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-13 20:57:23 -0500

    feat(error) Handling HTTP errors

--- app.js
+++ app.js
@@ -5,14 +5,29 @@ const url = api.API_CURRENT_WEATHER + "&query=37.8267,-122.4233"
 
 request({ url: url, json: true }, (error, response) => {
 
-    console.log(response.body)
-
-})
-
-request({ url: api.API_MAPBOX_GEO, json: true }, (error, response) => {
-
-    console.log(response.body)
+    if (error) {
+        console.log('Unable to connect to weather service!')
+    } else if(response.body.error) {
+        console.log('Unable to find location!')
+    } else {
+        console.log(response.body)
+    }
 
 })
 
 //Geocoding
+
+request({ url: api.API_MAPBOX_GEO, json: true }, (error, response) => {
+
+    if (error) {
+        console.log('Unable to connect to location services!')
+    } else if(response.body.features.lenght === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const lat = response.body.features[0].center[0]
+        const long = response.body.features[0].center[1]
+        console.log(lat, long)
+    }
+
+})
+
--- util/constants.js
+++ util/constants.js
@@ -1,12 +1,12 @@
-const API_KEY = ""
-const MAPBOX_ACCESS_TOKEN = ""
+const WEATHERSTACK_KEY = ""
+const MAPBOX_KEY = ""
 
 const API_BASE_URL = "http://api.weatherstack.com/"
-const API_CURRENT_WEATHER = API_BASE_URL + "current?access_key=" + API_KEY
-const API_HISTORICAL_WEATHER = API_BASE_URL + "historical?access_key=" + API_KEY
-const API_WEATHER_FORECAST = API_BASE_URL + "forecast?access_key=" + API_KEY
+const API_CURRENT_WEATHER = API_BASE_URL + "current?access_key=" + WEATHERSTACK_KEY
+const API_HISTORICAL_WEATHER = API_BASE_URL + "historical?access_key=" + WEATHERSTACK_KEY
+const API_WEATHER_FORECAST = API_BASE_URL + "forecast?access_key=" + WEATHERSTACK_KEY
 
-const API_MAPBOX_GEO = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + MAPBOX_ACCESS_TOKEN + "&limit=1"
+const API_MAPBOX_GEO = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + MAPBOX_KEY + "&limit=1"
 
 module.exports = {
     API_BASE_URL: API_BASE_URL,
