ab35ad5c9dbe2321a77fe688a4081cc803e167fb
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-14 18:46:10 -0500

    feat(callback) Geolocation callback function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const request = require('request')
-const api = require('./util/constants')
+const geocode = require('./util/geocode')
 
-const url = api.API_CURRENT_WEATHER + "&query=37.8267,-122.4233"
+/*const url = api.API_CURRENT_WEATHER + "&query=37.8267,-122.4233"
 
 request({ url: url, json: true }, (error, response) => {
 
@@ -13,21 +12,9 @@ request({ url: url, json: true }, (error, response) => {
         console.log(response.body)
     }
 
+})*/
+
+geocode('Querétaro', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
-
-//Geocoding
-
-request({ url: api.API_MAPBOX_GEO, json: true }, (error, response) => {
-
-    if (error) {
-        console.log('Unable to connect to location services!')
-    } else if(response.body.features.lenght === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const lat = response.body.features[0].center[0]
-        const long = response.body.features[0].center[1]
-        console.log(lat, long)
-    }
-
-})
-
