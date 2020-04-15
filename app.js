0d9c86502445221290e3125045610edbc4a4de43
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-14 19:43:17 -0500

    feat(process.argv) Set the location by terminal using process.argv

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const geocode = require('./util/geocode')
 const forecast = require('./util/forecast')
 
-geocode('Pachuca', (error, data) => {
+const location = process.argv[2]
 
-    if(error) {
-        return console.log('Error: ', error)
-    }
+if(!location) {
 
-    forecast( data.latitude, data.longitude, (error, forecastData) => {
+    console.log('Please provide a location')
+
+} else {
+
+    geocode(location, (error, data) => {
 
         if(error) {
             return console.log('Error: ', error)
         }
-
-        console.log(data.location)
-        console.log(forecastData)
+    
+        forecast( data.latitude, data.longitude, (error, forecastData) => {
+    
+            if(error) {
+                return console.log('Error: ', error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
     })
-})
+}
+
