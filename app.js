2073869ee48c1a8624ab88fb36f8e1ab75246fe5
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-15 18:14:41 -0500

    refactor(app) HTTP request withour library

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const geocode = require('./util/geocode')
 const forecast = require('./util/forecast')
 
+const https = require('https')
+const api = require('./util/constants')
+
+const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + api.MAPBOX_KEY + "&limit=1"
+
+const request = https.request(url, (response) => {
+
+    let data = ''
+
+    //register a handler
+    response.on('data', (chunk) => {
+        data = data + chunk.toString()
+        //console.log(chunk)
+    })
+    
+    //register a handler
+    response.on('end', () => {
+        const body = JSON.parse(data)
+        console.log(body)
+    }) 
+
+})
+
+request.on('error', (error) => {
+    console.log('An error ', error)
+})
+
+request.end()
+
 const location = process.argv[2]
 
 if(!location) {
