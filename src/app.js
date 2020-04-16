bc5bf45f30937e7305fe3d1d750f53b820385524
Jorge Elias <jorgea.eliasl7@gmail.com>
2020-04-15 19:03:40 -0500

    feat(paths) Ad routes to the web server

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ app.get('', (req, res) => {
     res.send('Hello express!')
 })
 
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather')
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
